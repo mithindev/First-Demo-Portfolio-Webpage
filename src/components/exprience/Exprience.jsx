1d483d31a25b553f5620b4c260cfb9058018dd37
mithindev <mithindev1@gmail.com>
2022-08-09 13:32:35 +0530

    finally commited

---
+++ src/components/exprience/Exprience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Exprience.css'
+
+const Exprience = () => {
+  return (
+    <div>Exprience</div>
+  )
+}
+
+export default Exprience
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Nav.css'
+
+const nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default nav
